8daae456c108498b93142e8241a98eb38ca9f192
rizikdhabre <rizikdhabre1802@hotmail.com>
2024-03-02 14:00:09 +0200

    organize styles

--- homeWork16/src/App.jsx
+++ homeWork16/src/App.jsx
@@ -1,5 +1,5 @@
 import Dashboard from "./components/Dashboard"
-import './App.css'
+import "./styles/App.css"
 
 const  App=()=> {
   return (
--- homeWork16/src/main.jsx
+++ homeWork16/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./styles/main.css"
 import Header from "./components/Header"
 import Title from "./components/Title.jsx"
 import Footer from './components/Footer.jsx'
